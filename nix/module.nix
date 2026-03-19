b3725c76d05bfea600ded5922d56e90d061dcef7
Francesco Dainese <franci.dainese@gmail.com>
2026-03-19 14:02:16 +0100

    feat(config): KACHE_CONFIG env

--- nix/module.nix
+++ nix/module.nix
@@ -191,6 +191,7 @@ in {
           };
           EnvironmentVariables = {
             KACHE_LOG = cfg.daemon.logLevel;
+            KACHE_CONFIG = configFile;
           };
           ThrottleInterval = 5;
         };
@@ -212,6 +213,7 @@ in {
         };
         environment = {
           KACHE_LOG = cfg.daemon.logLevel;
+          KACHE_CONFIG = configFile;
         };
       };
     })
